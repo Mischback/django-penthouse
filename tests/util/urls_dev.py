ef1a80e24d3be759955742666bd6d1979a57c189
Mischback <dev@mischback.de>
2024-09-26 21:50:18 +0200

    Make Django work

---
+++ tests/util/urls_dev.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2024 Mischback
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+
+"""Provides a minimum url configuration to run the app with Django project."""
+
+# Django imports
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+]
+
+try:
+    # django-debug-toolbar is only required during development
+    # external imports
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
+
+except ModuleNotFoundError:
+    # This catches the ModuleNotFoundError during testing
+    # django-debug-toolbar is just a development requirement
+    pass
