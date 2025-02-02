943acc0dc746c76aa4c02b04857c649f93c3973f
Mischback <dev@mischback.de>
2025-02-02 21:22:07 +0100

    Provide initial draft of meta progress chart

--- auxiliary/script/penthouse/tracker.ts
+++ auxiliary/script/penthouse/tracker.ts
@@ -1,5 +1,9 @@
+// FIXME: Convert to direct imports for treeshaking
+import Chart from "chart.js/auto";
+import "chartjs-adapter-date-fns";
+
 import { getDomElement } from "../utility";
-import { createLineChart } from "../utility/charts";
+// import { createLineChart } from "../utility/charts";
 
 /**
  * Create the visual representation of the meta datapoints.
@@ -7,7 +11,8 @@ import { createLineChart } from "../utility/charts";
  * This function is closely tied to the app's view and template.
  */
 export function createMetaDataChart(): void {
-  const table = getDomElement(null, "#meta-data-table");
+  const table = <HTMLTableElement>getDomElement(null, "#meta-data-table");
+  const canvas = <HTMLCanvasElement>getDomElement(null, "#meta-data-canvas");
 
   // grab raw data from the table
   let tableRow;
@@ -15,26 +20,69 @@ export function createMetaDataChart(): void {
   const ltcData = [];
   const ltsData = [];
   for (let i = 1; i < table.rows.length; i++) {
-    let thisLabel;
-    let thisLtc;
-    let thisLts;
     tableRow = table.rows[i];
 
     // the label is determined by the datapoints ``date``
-    thisLabel = getDomElement(tableRow, ".meta-date-raw").innerHTML;
+    // @ts-expect-error TS2345: Will work or catched by ``getDomElement()``
+    const thisLabel = getDomElement(tableRow, ".meta-date-raw").innerHTML;
+    const timestamp = parseInt(thisLabel, 10) * 1000;
 
     // we want LTC/LTS as datapoints
-    thisLtc = getDomElement(tableRow, ".meta-ltc-raw").innerHTML;
-    thisLts = getDomElement(tableRow, ".meta-lts-raw").innerHTML;
+    // @ts-expect-error TS2345: Will work or catched by ``getDomElement()``
+    const thisLtc = getDomElement(tableRow, ".meta-ltc-raw").innerHTML;
+    // @ts-expect-error TS2345: Will work or catched by ``getDomElement()``
+    const thisLts = getDomElement(tableRow, ".meta-lts-raw").innerHTML;
 
-    labels.push(thisLabel);
+    labels.push(timestamp);
     ltcData.push(thisLtc);
     ltsData.push(thisLts);
   }
 
-  console.log(labels);
-  console.log(ltcData);
-  console.log(ltsData);
+  // console.log(labels);
+  // console.log(ltcData);
+  // console.log(ltsData);
 
-  const chart = createLineChart("#meta-data-canvas", labels, ltcData);
+  // TODO: This still needs lots of work. Actually I would love to provide the
+  //       overall and common configuration of all charts in a central place
+  //       and only have the very specific bits here.
+  new Chart(canvas, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "LTC",
+          data: ltcData,
+          yAxisID: "y",
+        },
+        {
+          label: "LTS",
+          data: ltsData,
+          yAxisID: "y1",
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          type: "time",
+          time: {
+            unit: "day",
+          },
+        },
+        y: {
+          type: "logarithmic",
+          display: true,
+          position: "left",
+        },
+        y1: {
+          type: "linear",
+          display: true,
+          position: "right",
+        },
+      },
+    },
+  });
 }
