4f0f59049c99035b902bd23403a7f07232a62fa0
Mischback <dev@mischback.de>
2025-02-09 21:07:25 +0100

    Implement the required JS

--- auxiliary/script/penthouse.ts
+++ auxiliary/script/penthouse.ts
@@ -2,12 +2,14 @@ import {
   createMetaDataChart,
   createRunTrackerChart,
 } from "./penthouse/tracker";
+import { initializeCollapsibles } from "./utility/collapsible";
 
 /* Add a global event listener to apply the app-specific scripts. */
 document.addEventListener("DOMContentLoaded", () => {
   /* Run Tracker */
   if (document.getElementById("penthouse-tracker")) {
     createRunTrackerChart();
+    initializeCollapsibles();
   }
 
   /* Meta Tracker */
