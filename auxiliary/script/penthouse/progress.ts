c816ebdc672c789a2414e793f816792205b2265d
Mischback <dev@mischback.de>
2025-03-16 21:40:06 +0100

    Hide control elements in *view mode*

--- auxiliary/script/penthouse/progress.ts
+++ auxiliary/script/penthouse/progress.ts
@@ -11,32 +11,29 @@ export function initializeEditModeToggles(): void {
     const toggle = m.querySelector(".toggle-button input");
     if (!toggle) return;
 
-    const sectionList = m.querySelector(".ms-section-list");
-    if (!sectionList) return;
-
     // get the last state from localStorage and restore it
     const state = localStorage.getItem(`progress-edit-mode-state-${id}`);
     if (state === "active") {
-      sectionList.classList.remove("view-mode");
+      m.classList.remove("view-mode");
       toggle.checked = true;
     } else {
-      sectionList.classList.add("view-mode");
+      m.classList.add("view-mode");
       toggle.checked = false;
     }
 
     // add the event listener to the toggle button
-    toggle.addEventListener("click", () => toggleEditMode(sectionList, id));
+    toggle.addEventListener("click", () => toggleEditMode(m, id));
   });
 }
 
-function toggleEditMode(sectionList: HTMLElement, id: string): void {
-  const viewModeActive = sectionList.classList.contains("view-mode");
+function toggleEditMode(container: HTMLElement, id: string): void {
+  const viewModeActive = container.classList.contains("view-mode");
 
   if (viewModeActive) {
-    sectionList.classList.remove("view-mode");
+    container.classList.remove("view-mode");
     localStorage.setItem(`progress-edit-mode-state-${id}`, "active");
   } else {
-    sectionList.classList.add("view-mode");
+    container.classList.add("view-mode");
     localStorage.setItem(`progress-edit-mode-state-${id}`, "inactive");
   }
 }
