28c931dc6f6645fd77cbe4a38743df987978a25b
Mischback <dev@mischback.de>
2025-03-20 20:22:29 +0100

    Fetch basic setup from first iteration

---
+++ auxiliary/script/penthouse.ts
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Mischback
+// SPDX-License-Identifier: MIT
+// SPDX-FileType: SOURCE
+
+/* Add a global event listener to apply the app-specific scripts. */
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("performing the progressive enhancement");
+});
---
+++ penthouse/urls.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2024 Mischback
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+
+"""App-specific URL configuration."""
+
+# Django imports
+from django.urls import path
+
+app_name = "penthouse"
+
+urlpatterns = [
+]
