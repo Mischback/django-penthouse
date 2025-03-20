fb4feb3b810eded84289c61e6c17d2b2390bb802
Mischback <dev@mischback.de>
2025-03-20 22:35:30 +0100

    Add the Account model

---
+++ penthouse/models/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 Mischback
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+
+"""App-specific models."""
+
+# app imports
+from penthouse.models.account import Account  # noqa: F401
