b4d88b68595c64395833121663111f51b41cf4a2
Mischback <dev@mischback.de>
2025-04-23 16:53:32 +0200

    Provide required container ID

--- auxiliary/script/utility/index.ts
+++ auxiliary/script/utility/index.ts
@@ -2,8 +2,20 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileType: SOURCE
 
+/**
+ * Type definition for a setup function.
+ *
+ * This function is called on the elements and should do all necessary setup
+ * and initialization steps.
+ */
 export type setupFunc = (element: HTMLElement) => void;
 
+/**
+ * Apply a setup function to HTML elements.
+ *
+ * The elements are identified by a *query* and the ``setupFunc`` is called on
+ * each of them.
+ */
 export function applyProgressiveEnhancement(
   query: string,
   setupFunc: setupFunc,
